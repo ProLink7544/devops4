aa956f4fcab4905c8c7dcb4c514a7885e200201b
Protus <prongugi@gmail.com>
2022-11-10 11:34:34 +0300

    first commit

---
+++ button.js
@@ -0,0 +1,10 @@
+jQuery(document).ready(function () {
+    jQuery('button#SUBMIT').click(function() {
+        jQuery(this).hide();
+    });
+
+
+    jQuery('button.NO SUBMIT').click(function() {
+        jQuery(this).hide();
+    });
+});
